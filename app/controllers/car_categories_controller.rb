110255c204001e854f8269b1a47ab2e554dd7ad1
Alexandre Luiz Elias <alexandre.alpe@gmail.com>
2020-08-08 23:29:11 -0300

    view details OK

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -1,5 +1,9 @@
 class CarCategoriesController < ApplicationController
   def index
     @car_categories = CarCategory.all
-  end  
+  end
+
+  def show
+    @car_category = CarCategory.find(params[:id])
+  end
 end
