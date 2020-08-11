ca72ee050cd1cf6683c46decc09eb6ea434941b1
Alexandre Luiz Elias <alexandre.alpe@gmail.com>
2020-08-11 10:25:18 -0300

    Show details of a subsidiary

--- app/controllers/rental_subsidiaries_controller.rb
+++ app/controllers/rental_subsidiaries_controller.rb
@@ -2,4 +2,11 @@ class RentalSubsidiariesController < ApplicationController
   def index
     @rental_subisidiaries = RentalSubsidiary.all
   end
+
+  def show
+    #puts '==================='
+    #puts params
+    #puts '==================='
+    @rental_subisidiary = RentalSubsidiary.find(params[:id])
+  end
 end
--- spec/features/admin_view_subsidiaries_spec.rb
+++ spec/features/admin_view_subsidiaries_spec.rb
@@ -2,14 +2,36 @@ require 'rails_helper'
 
 feature 'Admin view subsidiaries' do
   scenario 'successfully' do
-    RentalSubsidiary.create!(name:'São Paulo', cnpj:'71.734.693/0001-94', adress:'Av. Paulista')
-    RentalSubsidiary.create!(name:'Recife', cnpj:'52.536.790/0001-28', adress:'Av. Conde de Boa Vista')
+    RentalSubsidiary.create!(name:'São Paulo', 
+                             cnpj:'71.734.693/0001-94', 
+                             adress:'Av. Paulista')
+    RentalSubsidiary.create!(name:'Recife', 
+                             cnpj:'52.536.790/0001-28', 
+                             adress:'Av. Conde de Boa Vista')
 
     visit root_path
     click_on 'Subsidiárias'
 
     expect(page).to have_content('São Paulo')
     expect(page).to have_content('Recife')
+  end
+
+  scenario 'and view details' do
+    RentalSubsidiary.create!(name:'São Paulo', 
+                             cnpj:'71.734.693/0001-94', 
+                             adress:'Av. Paulista')
+    RentalSubsidiary.create!(name:'Recife', 
+                             cnpj:'52.536.790/0001-28', 
+                             adress:'Av. Conde de Boa Vista')
+
+    visit root_path
+    click_on 'Subsidiárias'
+    click_on 'São Paulo'
+
+    expect(page).to have_content('São Paulo')
+    expect(page).to have_content('71.734.693/0001-94')
+    expect(page).to have_content('Av. Paulista')
+    expect(page).not_to have_content('Recife')
   end
 
 end
