981055a2ec4fe1822997bf0e15bf2a50bcb300c7
Alexandre Luiz Elias <alexandre.alpe@gmail.com>
2020-08-21 13:25:11 -0300

    Admin view all car models of a given category

--- spec/features/admin_view_car_categories_spec.rb
+++ spec/features/admin_view_car_categories_spec.rb
@@ -61,6 +61,37 @@ feature 'Admin view car categories' do
     expect(page).to have_content('Nenhuma categoria cadastrada')
   end
 
+  scenario 'and view all car models under that category' do
+    # Arrenge
+    car_category = CarCategory.create!(name: 'Basic', daily_rate: 20, 
+                                       car_insurance: 50, third_party_insurance: 100 )
+    CarModel.create!(name: 'Ka', year: 2019, 
+                     manufacturer: 'Ford', motorization: '1.0', 
+                     car_category: car_category , fuel_type: 'Flex')
+    CarModel.create!(name: 'Gol', year: 2020, 
+                     manufacturer: 'Volkswagen', motorization: '1.0', 
+                     car_category: car_category, fuel_type: 'Gasolina')
+    user = User.create!(name: 'Alexandre Elias', 
+                        email: 'alexandre@email.com', 
+                        password: '123456789')
+    
+    #Act
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Basic'
+
+    #Assert
+    expect(page).to have_content('Basic')
+    expect(page).to have_content('20')
+    expect(page).to have_content('50')
+    expect(page).to have_content('100')
+    expect(page).to have_content('Ka')
+    expect(page).to have_content('Gol')
+    expect(page).to have_content('Lista de modelos da categoria Basic')
+
+  end
+
   scenario 'and return to home page' do
     CarCategory.create!(name: 'Top', daily_rate: 105.5, car_insurance: 58.5,
                         third_party_insurance: 10.5)
